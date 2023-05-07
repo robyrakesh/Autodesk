8439f6e1d887e124a2b7bfd464aca66e90213bf3
mounika2107 <43798035+mounika2107@users.noreply.github.com>
2023-05-07 13:32:38 -0400

    fix allure reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
@@ -11,7 +12,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      // return require('./cypress/plugins/index.js')(on, config)
+      allureWriter(on, config)
     },
     baseUrl: 'https://www.autodesk.ca',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
