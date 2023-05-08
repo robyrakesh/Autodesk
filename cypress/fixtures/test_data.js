f666ea5de319903f666ca905730c320f1169953a
mounika2107 <43798035+mounika2107@users.noreply.github.com>
2023-05-08 12:48:16 -0400

    add test data

--- cypress/fixtures/test_data.js
+++ cypress/fixtures/test_data.js
@@ -3,6 +3,6 @@ import { faker } from '@faker-js/faker';
 export const userDetails = {
     firstName: 'Mounika',
     lastName:'P',
-    email:faker.internet.email('rem','test', 'gib.com'),
+    email:faker.internet.email('emailtest189283','test', 'gib.com'),
     password:'Test@1234'
 }
